bd5fb4cb2cea8fc8fd870dd2df36ba5764058faf
MauricioGT2024 <senasilvana192@gmail.com>
2025-01-14 00:16:54 +0000

    13/01/25

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
+import React from 'react';
+import Card from '../components/Card/Card';
 import HomeDetails from "../components/Inicio/HomeDetails";
 
-
-function Home() {
+const Home = () => {
   return (
     <>
-    <HomeDetails/>
+      <HomeDetails/>
+      <div className="card-grid">
+        <Card title="Card 1" description="Description 1" image="/path/to/image1.jpg" />
+        <Card title="Card 2" description="Description 2" image="/path/to/image2.jpg" />
+        <Card title="Card 3" description="Description 3" image="/path/to/image3.jpg" />
+        {/* ...otros cards... */}
+      </div>
     </>
   );
-}
+};
 
 export default Home;
