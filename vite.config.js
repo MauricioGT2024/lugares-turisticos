0fc0bf4aa1ee8ff90d4c505dbeddd6fb69bf7d66
MauricioGT2024 <senasilvana192@gmail.com>
2025-01-16 12:42:59 +0000

    16/01/25

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,27 @@ export default defineConfig({
   plugins: [react()],
   build: {
     target: 'esnext',
-    minify: 'terser', // Cambiamos 'terser' por 'esbuild'
+    minify: 'terser',
     cssMinify: true,
     rollupOptions: {
       output: {
         manualChunks: {
-          'vendor': ['react', 'react-dom', 'react-router-dom']
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'components': [
+            './src/components/Provincia/Provincia.jsx',
+            './src/components/Catamarca/Catamarca.jsx',
+            './src/components/Fiambala/Fiambala.jsx',
+            './src/components/Tinogasta/Tinogasta.jsx',
+            './src/components/Antofagasta-De-La-Sierra/AntofagastaDeLaSierra.jsx',
+            './src/components/Hospedaje/Hospedaje.jsx'
+          ]
         }
       }
-    }
+    },
+    chunkSizeWarningLimit: 1000
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom']
   }
 })
 
