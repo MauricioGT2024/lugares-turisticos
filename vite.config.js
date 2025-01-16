56fa2d82b4a4cebb640c65988bad0abfac908958
MauricioGT2024 <senasilvana192@gmail.com>
2025-01-16 12:23:54 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-      target: 'esnext',
-      minify: 'terser',
-      cssMinify: true,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            'vendor': ['react', 'react-dom', 'react-router-dom']
-          }
+  build: {
+    target: 'esnext',
+    minify: 'terser', // Cambiamos 'terser' por 'esbuild'
+    cssMinify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom']
         }
       }
     }
-  })
+  }
+})
 
